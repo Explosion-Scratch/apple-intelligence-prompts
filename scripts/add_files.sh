cb298e073288ad842e9a56554ec8546d3e3c1c8f
Explosion Implosion <explosion.scratch@gmail.com>
2024-08-14 09:57:08 -0500

    Code updates

--- scripts/add_files.sh
+++ scripts/add_files.sh
@@ -2,12 +2,15 @@
 
 # Run from the root of the repo
 fd --type f -g "*.json" . -X git add -v {}
+fd --type f -g "*.cryptex_info" . -X git add -v {}
 fd --type f -g "*Manifest.plist" . -X git add -v {}
 fd --type f -g "*Restore.plist" . -X git add -v {}
 fd --type f -g "*Info.plist" . -X git add -v {}
 fd --type f -g "*GenerativeModels.xml" . -X git add -v {}
 fd --type f -g "*Configuration.xml" . -X git add -v {}
 fd --type f -g "*Version.plist" . -X git add -v {}
+fd --type f -g "*.pbtxt" . -X git add -v {}
+fd --type f -g "receipt.plist" . -X git add -v {}
 git add -v scripts
 git add "*.md"
 git add summary.json
